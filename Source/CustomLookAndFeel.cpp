cc4d4adc48ffb8e99598f51f4c9f25f12e472a9b
aimeedowell <aimeedowell24@gmail.com>
2020-09-09 10:07:29 +0100

    [build] CustomLookAndFeel: add class and inherit from juce::LookAndFeel_V4

---
+++ Source/CustomLookAndFeel.cpp
@@ -0,0 +1,20 @@
+//
+//  CustomLookAndFeel.cpp
+//  Hepatica Reverb - VST3
+//
+//  Created by Aimee Dowell on 08/09/2020.
+//  Copyright © 2020 Aimee Dowell. All rights reserved.
+//
+#include "CustomLookAndFeel.h"
+#include <stdio.h>
+using namespace juce;
+
+CustomLookAndFeel::CustomLookAndFeel()
+{
+    
+}
+
+CustomLookAndFeel::~CustomLookAndFeel()
+{
+    
+}
---
+++ Source/CustomLookAndFeel.h
@@ -0,0 +1,23 @@
+//
+//  CustomLookAndFeel.h
+//  Hepatica Reverb
+//
+//  Created by Aimee Dowell on 08/09/2020.
+//  Copyright © 2020 Aimee Dowell. All rights reserved.
+//
+
+#ifndef CustomLookAndFeel_h
+#define CustomLookAndFeel_h
+
+#include <JuceHeader.h>
+
+class CustomLookAndFeel
+    : public juce::LookAndFeel_V4
+{
+public:
+    CustomLookAndFeel();
+    ~CustomLookAndFeel();
+    
+};
+#endif /* CustomLookAndFeel_h */
+
