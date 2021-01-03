ed73389919cfc7c9692816e98176d920e31008f5
aimeedowell <aimeedowell24@gmail.com>
2021-01-03 20:36:39 +0000

    [internal] create all pass filter class

---
+++ Source/AllPassFilter.h
@@ -0,0 +1,53 @@
+/*
+  ==============================================================================
+
+    AllPassFilter.h
+    Created: 3 Jan 2021 1:00:39pm
+    Author:  Aimee Dowell
+
+  ==============================================================================
+*/
+
+#pragma once
+#include <JuceHeader.h>
+
+using namespace juce;
+
+class AllPassFilter
+{
+public:
+    AllPassFilter() noexcept {}
+
+    void setSize (const int size)
+    {
+        if (size != bufferSize)
+        {
+            bufferIndex = 0;
+            buffer.malloc (size);
+            bufferSize = size;
+        }
+
+        clear();
+    }
+
+    void clear() noexcept
+    {
+        buffer.clear ((size_t) bufferSize);
+    }
+
+    float process (const float input) noexcept
+    {
+        const float bufferedValue = buffer [bufferIndex];
+        float temp = input + (bufferedValue * 0.5f);
+        JUCE_UNDENORMALISE (temp);
+        buffer [bufferIndex] = temp;
+        bufferIndex = (bufferIndex + 1) % bufferSize;
+        return bufferedValue - input;
+    }
+
+private:
+    HeapBlock<float> buffer;
+    int bufferSize = 0, bufferIndex = 0;
+
+    JUCE_DECLARE_NON_COPYABLE (AllPassFilter)
+};
